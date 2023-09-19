2fa5592b3a47bbcd0a733a41fea3c6af71e6aca7
Bob Ros <bob-ros@t-online.de>
2023-09-19 15:12:21 +0200

    add os start scripts

--- src/midirec.sh
+++ src/midirec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Windows start script for midirec.py https://gitlab.com/bob-ros2/midistuff
+# Shell start script for midirec.py https://gitlab.com/bob-ros2/midistuff
 
 echo -n "Do you want to start a single track recording (0) or record multiple tracks (1)? Choose 0 or 1: "
 read in

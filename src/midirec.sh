38d29c0b29eed171462d39a43e7c2f06e197777f
Bob Ros <bob-ros@t-online.de>
2023-09-19 15:19:56 +0200

    add os start scripts

--- src/midirec.sh
+++ src/midirec.sh
@@ -4,8 +4,5 @@
 echo -n "Do you want to start a single track recording (0) or record multiple tracks (1)? Choose 0 or 1: "
 read in
 
-if [ "$in" = "0" ]; then
-  python midirec.py
-elif [ "$in" = "1" ]; then
-  python midirec.py -a 30
-fi
+[ "$in" = "0" ] && python midirec.py
+[ "$in" = "1" ] && python midirec.py -a 30
